56e30bec50a6f9f4be60e97993c8c7378d6e19ca
RishitaSolanki <134220637+RishitaSolanki@users.noreply.github.com>
2025-12-19 12:19:17 +0530

    [Rishita] Datatype and operator in C#

---
+++ core-cshrap-practice/gcr-codebase/programming-elements/Datatype.cs
@@ -0,0 +1,53 @@
+//Datatype
+
+  using System;
+
+  public class Datatype{
+
+  public static void Main(string[] args){
+  
+  int a=10;
+    Console.WriteLine(a);
+  
+  long b=1000;
+    Console.WriteLine(b);
+
+  
+  float c= 1.02f;
+    Console.WriteLine(c);
+
+  
+  double d=22.05;
+    Console.WriteLine(d);
+
+  
+  char e='G';
+    Console.WriteLine(e);
+
+  
+  bool isf= false;
+    Console.WriteLine(isf);
+
+  
+  String name="Rishita";
+    Console.WriteLine(name);
+
+  
+  //Implicit Typecasting
+  
+  int num=10;
+  double pointnum=num;
+    Console.WriteLine(pointnum);
+
+  
+  //Explicit Typecasting
+  
+  double n=11.55;
+  int m=(int) n;
+    Console.WriteLine(m);
+
+  
+
+}
+
+}
