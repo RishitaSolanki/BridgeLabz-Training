110cd9834774c816e301875a0d681f888da52aec
RishitaSolanki <134220637+RishitaSolanki@users.noreply.github.com>
2025-12-28 21:49:14 +0530

    [Rishita] Problem solved in C# String level2

---
+++ core-cshrap-practice/gcr-codebase/c#strings/level-2/CountVowelsAndConsonants.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectOne.strings
+{
+    public class CountVowelsAndConsonants
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter a string: ");
+            string text = Console.ReadLine().ToLower();
+
+            int vowel = 0;
+            int consonant = 0;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char ch = text[i];
+
+                if (ch >= 'a' && ch <= 'z')
+                {
+                    if (ch == 'a' || ch == 'e' || ch == 'i' ||
+                        ch == 'o' || ch == 'u')
+                    {
+                        vowel++;
+                    }
+                    else
+                    {
+                        consonant++;
+                    }
+                }
+            }
+
+            Console.WriteLine("Vowels: " + vowel);
+            Console.WriteLine("Consonants: " + consonant);
+        }
+    }
+}
