f52fa4f0452eae1f0b67c64d7d506795ebfd46e9
RishitaSolanki <134220637+RishitaSolanki@users.noreply.github.com>
2026-01-03 13:01:18 +0530

    [Rishita] Problem solved in C# Class and Objects

---
+++ oops-cshrap-practice/gcr-codebase/c#classandobjects/BookDetails.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace projectTwo
+{
+    public class BookDetails
+    {
+        string Title;
+        string Author;
+        double Price;
+
+        //Parmeterized constructor
+        public BookDetails(string enteredTitle, string enteredAuthor, double enteredPrice)
+        {
+            this.Title = enteredTitle;
+            this.Author = enteredAuthor;
+            this.Price = enteredPrice;
+
+        }
+
+        public void ShowBookDetails()
+        {
+            Console.WriteLine("Book Details are given below");
+            Console.WriteLine("Title of the book is : " + Title);
+            Console.WriteLine("Author of the book is : " + Author);
+            Console.WriteLine("Price of the book is : " + Price);
+
+        }
+
+        //MAIN METHOD
+        static void Main(string[] args)
+        {
+            BookDetails B1 = new BookDetails("Dev ki kahaniya", "Rishita", 300);
+            B1.ShowBookDetails();
+        }
+    }
+}
