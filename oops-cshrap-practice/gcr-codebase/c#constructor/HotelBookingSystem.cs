d6f12963f6679f584b660b193fa7cd0f2a53748c
RishitaSolanki <134220637+RishitaSolanki@users.noreply.github.com>
2026-01-03 12:44:07 +0530

    [Rishita] Problem solved in C# Constructor

---
+++ oops-cshrap-practice/gcr-codebase/c#constructor/HotelBookingSystem.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace projectTwo
+{
+    public class HotelBookingSystem
+    {
+        public string guestName;
+        public string roomType;
+        public string nights;
+
+        //DEFAULT CONSTRUCTOR
+        public HotelBookingSystem()
+        {
+            guestName = "Rishita";
+            roomType = "Basic";
+            nights = "2 Night";
+        }
+
+        //PARAMETERIZED CONSTRUCTOR AND TAKING USER INPUT
+        public HotelBookingSystem(string GuestName, string RoomType, string Nights)
+        {
+            this.guestName = GuestName;
+            this.roomType = RoomType;
+            this.nights = Nights;
+        }
+
+        //COPY CONSTRUCTOR
+        public HotelBookingSystem(HotelBookingSystem other)
+        {
+            guestName = other.guestName;
+            roomType = other.roomType;
+            nights = other.nights;
+        }
+
+        //MAIN METHOD
+
+        public static void Main(string[] args)
+        {
+            //defalut constructor calling
+            HotelBookingSystem B1=new HotelBookingSystem();
+
+            Console.WriteLine(B1.guestName + "," + B1.roomType + "," + B1.nights);
+
+
+            //paramterized constructor calling 
+            HotelBookingSystem B2 = new HotelBookingSystem("Rama" , "VIP" ,"4 nights");
+
+            Console.WriteLine(B2.guestName + "," + B2.roomType + "," + B2.nights);
+
+
+            //copy constructor calling
+            HotelBookingSystem B3 = new HotelBookingSystem(B2);
+
+            Console.WriteLine(B3.guestName + "," + B3.roomType + "," + B3.nights);
+
+        }
+
+    }
+}
---
+++ oops-cshrap-practice/gcr-codebase/c#constructor/LibraryBookSystem.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace projectTwo
+{
+    public class LibraryBookSystem
+    {
+        public string title;
+        public string author;
+        public double price;
+        public bool IsBookAvailable;
+
+        // Parameterized Constructor
+        public LibraryBookSystem(string Title, string Author, double Price)
+        {
+            this.title = Title;
+            this.author = Author;
+            this.price = Price;
+            this.IsBookAvailable = true;   
+        }
+
+
+        //Method top borrow book
+        public  void BorrowBook()
+        {
+            if (IsBookAvailable)
+            {
+                IsBookAvailable = false;
+                Console.WriteLine("You have successfully borrowed the book: " + title);
+            }
+            else
+            {
+                Console.WriteLine("Sorry, the book is not available");
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            LibraryBookSystem book1 = new LibraryBookSystem("The Alchemist", "Paulo Coelho", 399);
+
+            book1.BorrowBook(); 
+            
+            book1.BorrowBook();   
+        
+
+    }
+    }
+}
