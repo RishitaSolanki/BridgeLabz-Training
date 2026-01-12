9ef4333e15c8208bb809c5bff35373de26ed971f
RishitaSolanki <134220637+RishitaSolanki@users.noreply.github.com>
2026-01-12 22:17:21 +0530

    [Rishita] Solved Metal Factory Pipe Cutting Problem

---
+++ dsa-csharp-practice/scenario-based/MetalFactoryPipeCutting/MetalFactoryPipeCuttingMain.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScenarioBased.MetalFactoryPipeCutting
+{
+    //Maximize revenue from cutting metal rods of different sizes and values.
+    public class MetalFactoryPipeCuttingMain
+    {
+        //Main method
+           public static void Main(string[] args)
+            {
+                new RodCuttingUtility().Start();
+            }
+        }
+    }
+
---
+++ dsa-csharp-practice/scenario-based/MetalFactoryPipeCutting/OptimizedRodCutting.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScenarioBased.MetalFactoryPipeCutting
+{
+    public class OptimizedRodCutting : IRodCuttingStrategy
+    {
+        public int GetMaxRevenue(int len, int[] prices)
+        {
+            int[] arr = new int[len + 1];
+
+            for (int i = 1; i < len; i++)
+            {
+                int max = 0;
+
+                for (int cut = 1; cut <= i; cut++)
+                {
+                    max = Math.Max(max, prices[cut] + arr[i - cut]);
+                }
+
+                arr[i] = max;
+            }
+
+            return arr[len];
+        }
+    }
+}
