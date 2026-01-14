62fa4099472f8e7c39f794bb9056f66263848a10
RishitaSolanki <134220637+RishitaSolanki@users.noreply.github.com>
2026-01-14 10:50:06 +0530

    [Rishita] Solved Movie Scheduler Manager Problem

---
+++ dsa-csharp-practice/scenario-based/MovieScheduleManager/CinemaManager.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScenarioBased.MovieScheduleManager
+{
+    public class CinemaManager
+    {
+            private IMovieOperation cinemaServices;
+
+        //constructor
+            public CinemaManager(IMovieOperation cinemaServices)
+            {
+                this.cinemaServices = cinemaServices;
+            }
+
+            public void ShowMenu()
+            {
+                while (true)
+                {
+                    Console.WriteLine("-CinemaTime Menu-");
+
+                    Console.WriteLine("1. Add Movie");
+
+                    Console.WriteLine("2. Search Movie");
+
+                    Console.WriteLine("3. Display All Movies");
+
+                    Console.WriteLine("4. Exit");
+
+                    Console.WriteLine();
+
+                    Console.Write("Enter your choice: ");
+
+                    int select = Convert.ToInt32(Console.ReadLine());
+
+                    switch (select)
+                    {
+                        case 1:
+                            Console.Write("Enter movie Name: ");
+                            string Name = Console.ReadLine();
+
+                            Console.Write("Enter show time: ");
+                            string time = Console.ReadLine();
+
+                        ECinema eCinema = new ECinema(Name, time);
+
+                            cinemaServices.AddMovie(eCinema);
+
+                            Console.WriteLine();
+                            break;
+
+                        case 2:
+                            Console.Write("Enter keyword to search: ");
+
+                            string typeAnyWord = Console.ReadLine();
+
+                            cinemaServices.SearchMovie(typeAnyWord);
+
+                            Console.WriteLine();
+                            break;
+
+                        case 3:
+
+                              cinemaServices.DisplayAllMovies();
+
+                             Console.WriteLine();
+                            break;
+
+                        case 4:
+                            Console.WriteLine("Thank you!");
+                            return;
+
+                        default:
+                            Console.ForegroundColor= ConsoleColor.Red;
+                            Console.WriteLine("Invalid choice. Try again.");
+                            Console.ResetColor();
+                            break;
+                    }
+                }
+            }
+        }
+}
---
+++ dsa-csharp-practice/scenario-based/MovieScheduleManager/ECinema.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScenarioBased.MovieScheduleManager
+{
+    //encapsulated class 
+    public class ECinema
+    {
+        private string movieName;
+        private string movieShowTime;
+
+
+        //constructor
+
+        public ECinema(string movieName, string movieShowTime)
+        {
+            this.movieName = movieName;
+            this.movieShowTime = movieShowTime;
+        }
+
+
+        public string MovieName
+        {
+            get { return movieName; }
+            set { movieName = value; }
+        }
+
+
+
+        public string MovieShowTime
+        {
+            get { return movieShowTime; }
+            set { movieShowTime = value; }
+        }
+
+    }
+}
+
