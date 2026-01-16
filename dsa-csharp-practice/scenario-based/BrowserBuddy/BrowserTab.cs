a053cf435223bfb8d88670f06d19e9fa9a4fcf7e
RishitaSolanki <134220637+RishitaSolanki@users.noreply.github.com>
2026-01-16 15:36:53 +0530

    [Rishita] Solved BrowserBuddy – Tab History Manager Problem

---
+++ dsa-csharp-practice/scenario-based/BrowserBuddy/BrowserTab.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScenarioBased.BrowserBuddy
+{
+    public class BrowserTab
+    {
+        private string url;
+        private BrowserTab next;
+        private BrowserTab previous;
+        public BrowserTab Next
+        {
+            get { return next; }
+            set { next = value; }
+        }
+        public BrowserTab Previous
+        {
+            get { return previous; }
+            set { next = value; }
+        }
+        public string Url
+        {
+            get { return url; }
+            set { url = value;}
+        }
+        public BrowserTab(string url)
+        {
+            Url = url;
+            Next = null;
+            Previous = null;
+        }
+    }
+}
+
---
+++ dsa-csharp-practice/scenario-based/BrowserBuddy/ITaskHistoryManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScenarioBased.BrowserBuddy
+{
+    public interface ITaskHistoryManager
+    {
+            void VisitPage(string url);
+            void GoBack();
+            void GoForward();
+            void CloseTab();
+            void RestoreTabs();
+            void ShowHistory();
+
+        }
+    }
+
