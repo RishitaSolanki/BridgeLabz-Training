37b8d07ca90f2b94b1a8528534bec5ded29d62c9
RishitaSolanki <134220637+RishitaSolanki@users.noreply.github.com>
2026-01-17 15:21:32 +0530

    [Rishita] Solved Fitness Manager Problem

---
+++ dsa-csharp-practice/scenario-based/FitnessManager/FitnessManagerMain.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScenarioBased.FitnessManager
+{
+    public class FitnessManagerMain
+    {
+        public static void Main(string[] args)
+        {
+            FitnessManagerMenu menu = new FitnessManagerMenu();
+            menu.Start();
+        }
+    }
+
+}
---
+++ dsa-csharp-practice/scenario-based/FitnessManager/IFitnessManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScenarioBased.FitnessManager
+{
+    public interface IFitnessManager
+    {
+        void LogUserSteps(string name, int steps);
+        void UpdateRankings();
+        void ShowLeaderboard();
+    }
+}
