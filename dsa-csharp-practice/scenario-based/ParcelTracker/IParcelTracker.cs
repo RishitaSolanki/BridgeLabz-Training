c0913a2f65283e5721bd2a9b4d17da35df531e4e
RishitaSolanki <134220637+RishitaSolanki@users.noreply.github.com>
2026-01-21 15:41:20 +0530

    [Rishita] Solved ParcelTracker – Delivery Chain Management  Problem

---
+++ dsa-csharp-practice/scenario-based/ParcelTracker/IParcelTracker.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScenarioBased.ParcelTracker
+{
+    public interface IParcelTracker
+    {
+        void AddStage(string stage);
+        void AddCheckpoint(string afterStage, string newStage);
+        void TrackForward();
+        void MarkLost(string stage);
+    }
+}
+
---
+++ dsa-csharp-practice/scenario-based/ParcelTracker/ParcelNode.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScenarioBased.ParcelTracker
+{
+    public class ParcelNode
+    {
+         private string stage;
+            private ParcelNode next;
+
+            public ParcelNode(string stage)
+            {
+                this.stage = stage;
+                next = null;
+            }
+
+            public string Stage
+            {
+                get { return stage; }
+                set { stage = value; }
+            }
+
+            public ParcelNode Next
+            {
+                get { return next; }
+                set { next = value; }
+            }
+        }
+    }
+
