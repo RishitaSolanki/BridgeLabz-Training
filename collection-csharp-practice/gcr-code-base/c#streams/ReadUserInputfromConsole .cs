6f5f2234bb8ffe361550f0ee21cdd1dc67d148a5
RishitaSolanki <134220637+RishitaSolanki@users.noreply.github.com>
2026-01-22 12:00:38 +0530

    [Rishita] Problem solved in c# streams

---
+++ collection-csharp-practice/gcr-code-base/c#streams/ReadUserInputfromConsole .cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectFour.Stream
+{
+    //Read User Input from Console 
+    public class ReadUserInputfromConsole
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Age: ");
+                string age = Console.ReadLine();
+
+                Console.Write("Language: ");
+                string lang = Console.ReadLine();
+
+                using (StreamWriter sw = new StreamWriter("user.txt"))
+                {
+                    sw.WriteLine(name);
+                    sw.WriteLine(age);
+                    sw.WriteLine(lang);
+                }
+
+                Console.WriteLine("Data saved");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
