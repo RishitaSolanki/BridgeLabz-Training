c00f137545d207e4e117eff513d3fe99c9c68d4f
RishitaSolanki <134220637+RishitaSolanki@users.noreply.github.com>
2026-01-28 16:05:49 +0530

    [Rishita] Solved Edu Result Problem

---
+++ dsa-csharp-practice/scenario-based/EduResults/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace scneariobased.EduResults
+{
+    public class Student
+    {
+       
+        public string Name { get; set; }
+        public int Score { get; set; }
+        public string District { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} ({District}) - {Score}";
+        }
+    }
+}
+
