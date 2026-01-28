bc3b3ca7031b61a837708f0e776eb0b8ab3c8fb5
RishitaSolanki <134220637+RishitaSolanki@users.noreply.github.com>
2026-01-28 16:34:12 +0530

    [Rishita] Solved AmbulanceRoute – Emergency Patient Navigation  Problem

---
+++ dsa-csharp-practice/scenario-based/AmbulanceRoute/IHospitalNavigation.cs
@@ -0,0 +1,19 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace scneariobased.AmbulanceRoute
+{
+    public interface IHospitalNavigation
+    {
+      
+            void AddUnit(string name);
+            void RemoveUnit(string name);
+            string FindNextAvailable(string startingPoint);
+            void SetAvailability(string name, bool isAvailable);
+            void DisplayRoute();
+        }
+    }
+
